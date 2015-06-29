6369e0909cd8c9f4b82998c33ea851e382e35a20
unknown <blue9@RM02-09.fasfg.local>
2015-06-29 15:10:47 +0100

    Test

--- Assignment/CourseBooking/SeatPlan.cs
+++ Assignment/CourseBooking/SeatPlan.cs
@@ -19,7 +19,7 @@ namespace CourseBooking
             string date;
             string price;
             string seats;
-            int counter = 0;
+            int counter = 1;
             label1.Text = list.Text;
            
             for (int i = 0; i < text.Length; i = i + 4 )
